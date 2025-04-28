bb74d860619871bfbdb02af228498d3b71e18ba4
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-28 13:24:28 -0400

    Started working on it.

---
+++ Accounts/src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class Transaction {
+        private String date;
+        private String time;
+        private String description;
+        private String vendor;
+        private double amount;
+
+        public Transaction(String date, String time, String description, String vendor, double amount) {
+            this.date = date;
+            this.time = time;
+            this.description = description;
+            this.vendor = vendor;
+            this.amount = amount;
+        }
+
+        // Getters and toString()
+        public String getDate() { return date; }
+        public String getTime() { return time; }
+        public String getDescription() { return description; }
+        public String getVendor() { return vendor; }
+        public double getAmount() { return amount; }
+
+        @Override
+        public String toString() {
+            return String.format("%s | %s | %s | %s | %.2f", date, time, description, vendor, amount);
+        }
+    }
+
+}
